71a905c55336181f0b9dd41fb330e0e37c094cab
Anirudh Gupta <anirudhgupta.ani0711@gmail.com>
2024-03-20 18:56:39 +0530

    adding images and main code

---
+++ robot_main.py
@@ -0,0 +1,76 @@
+import cv2
+import _thread
+from robotspeech import linearize
+
+text=input("Enter text: ")
+meta_text=''
+waitempty, waitfullstop, waitchar=0,0,0
+import subprocess
+
+def espeak(text: str, pitch: int=50) -> int:
+    """ Use espeak to convert text to speech. """
+    return subprocess.run(['espeak', f'-p {pitch}', text]).returncode
+
+_thread.start_new_thread(espeak, (text,) )
+meta_text='         '+linearize(text)+'          '
+print(meta_text)
+waitchar,waitfullstop,waitempty=40,30,30 
+
+mouthdict={
+    ' ':'robot_m.png',
+    '.':'robot_h.png',
+    '!':'robot_h.png',
+    '?':'robot_h.png',
+    ',':'robot_h.png',
+    ', ':'robot_h.png',
+    'a':'robot_u.png',
+    'b':'robot_e.png',
+    'c':'robot_e.png',
+    'd':'robot_h.png',
+    'e':'robot_e.png',
+    'f':'robot_m.png',
+    'g':'robot_u.png',
+    'h':'robot_h.png',
+    'i':'robot_h.png',
+    'j':'robot_o.png',
+    'k':'robot_o.png',
+    'l':'robot_s.png',
+    'm':'robot_m.png',
+    'n':'robot_m.png',
+    'o':'robot_o.png',
+    'p':'robot_e.png',
+    'q':'robot_s.png',
+    'r':'robot_o.png',
+    's':'robot_s.png',
+    't':'robot_e.png',
+    'u':'robot_u.png',
+    'v':'robot_s.png',
+    'w':'robot_s.png',
+    'x':'robot_s.png',
+    'y':'robot_s.png',
+    'z':'robot_s.png'
+}
+for char in meta_text:
+    if char in mouthdict:
+        if char==' ':
+            mouth=mouthdict[char]
+            print(mouth)
+            cv2.imshow(f"{char}", cv2.imread("./images/"+mouth))
+            cv2.waitKey(waitempty) 
+            cv2.destroyAllWindows()
+        elif char in ['.','!','?']:
+            mouth=mouthdict[char]
+            print(mouth)
+            cv2.imshow(f"{char}", cv2.imread("./images/"+mouth))
+            cv2.waitKey(8*waitfullstop) 
+            cv2.destroyAllWindows()
+        elif char in [',',', ']:
+            mouth=mouthdict[char]
+            print(mouth)
+            cv2.imshow(f"{char}", cv2.imread("./images/"+mouth))
+            cv2.waitKey(waitfullstop*4) 
+        else:
+            mouth=mouthdict[char]
+            print(mouth)
+            cv2.imshow(f"{char}", cv2.imread("./images/"+mouth))
+            cv2.waitKey(waitchar)
